e17d043ad556de9c3afb620bde324990cbc08682
hhannaee <hannakarkkainen@hotmail.com>
2024-04-09 14:02:27 +0300

    asetus

--- frontend/src/toiminnot/Asetukset.js
+++ frontend/src/toiminnot/Asetukset.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Asetukset = () => {
+  return (
+    <div>
+      <h2>Asetukset</h2>
+      <Link to="/uusituote">Luo uusi tuote</Link>
+      <Link to="/tulostus">Luo uusi tuote</Link>
+    </div>
+  );
+};
 
 export {Asetukset}
