a4ee0ccd3cfaa000c3e1dd2d95280a31cddc8841
hhannaee <hannakarkkainen@hotmail.com>
2024-04-10 13:53:50 +0300

    tulostus

--- frontend/src/toiminnot/Navigointi.js
+++ frontend/src/toiminnot/Navigointi.js
@@ -3,6 +3,10 @@ import { NavLink } from "react-router-dom";
 import '../styles/Navigointi.css';
 
 const Navigointi = () => {
+    const tulostaSivunNakyma = () => {
+        window.print(); 
+    }
+
     return(
         <nav>
             <div>
@@ -11,10 +15,9 @@ const Navigointi = () => {
             <div>
                 <NavLink className={"navLink"} to={"/UusiTuote"}>Lisää tuote</NavLink>
             </div>
-
+            <div className={"navLink"} onClick={tulostaSivunNakyma}>Tulosta</div> 
         </nav>
     )
-
 }
 
 export {Navigointi}
